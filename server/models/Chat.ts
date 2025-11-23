e5fbb32791160f14381720648d47e759b8188b27
nestapp70-max <nestapp70@gmail.com>
2025-11-23 14:40:23 +0530

    Update Chat.ts

--- server/models/Chat.ts
+++ server/models/Chat.ts
@@ -1,1 +1,6 @@
-// Chat model placeholder
+import mongoose from "mongoose";
+const ChatSchema = new mongoose.Schema({
+  room: String,
+  messages: [{ from: String, text: String, time: Date }]
+});
+export default mongoose.model("Chat", ChatSchema);

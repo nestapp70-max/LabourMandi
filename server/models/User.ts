21d8ddec52a6eadc0daab74a154449218a4172a4
nestapp70-max <nestapp70@gmail.com>
2025-11-23 14:41:34 +0530

    Update User.ts

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,1 +1,8 @@
-// User model placeholder
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["customer", "technician"] }
+});
+export default mongoose.model("User", UserSchema);

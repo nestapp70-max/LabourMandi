195eea3d3dd205b6f139417dda0a3570096c71b1
nestapp70-max <nestapp70@gmail.com>
2025-11-23 14:48:10 +0530

    Update chat.socket.ts

--- server/chat.socket.ts
+++ server/chat.socket.ts
@@ -1,1 +1,18 @@
-// chat.socket.ts placeholder
+import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
+
+export const setupChatSocket = (server: any) => {
+  const io = new Server(server, {
+    cors: { origin: process.env.FRONTEND_URL, credentials: true }
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("join", (room: string) => socket.join(room));
+
+    socket.on("message", (data) => {
+      io.to(data.room).emit("message", data);
+    });
+  });
+
+  console.log("Socket.io Live");
+};

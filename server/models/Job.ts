d5e06691f53870551335a53b3a99127616f0afdd
nestapp70-max <nestapp70@gmail.com>
2025-11-23 14:40:48 +0530

    Update Job.ts

--- server/models/Job.ts
+++ server/models/Job.ts
@@ -1,1 +1,9 @@
-// Job model placeholder
+import mongoose from "mongoose";
+const JobSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  description: String,
+  location: String,
+  amount: Number,
+  status: { type: String, default: "open" }
+});
+export default mongoose.model("Job", JobSchema);
